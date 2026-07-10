99969f7f2f79914d38116f78d1b95d1d9a248dda
AprioriDev <apriorifox.dev@gmail.com>
2026-07-10 17:17:58 +0600

    fix: catalog page with products filtration

--- src/pages/catalog/ui/Catalog.tsx
+++ src/pages/catalog/ui/Catalog.tsx
@@ -1,52 +1,14 @@
-"use client";
-
-import { useProductsStore } from "@/src/entities/catalog/model/store";
-import ItemCard from "@/src/features/ItemCard";
+import { Suspense } from "react";
+import { Products } from "@/src/widgets/catalog";
 import { Wrapper } from "@/src/shared/ui/Wrapper";
-import { CategoryFilters } from "@/src/widgets/categoriesFilter/ui/CategoryFilters";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useMemo, useState } from "react";
 
 export const Catalog = () => {
-  const { products, getProducts, isLoading } = useProductsStore();
-  const searchParams = useSearchParams();
-
-  const category = searchParams?.get("category");
-  const initialState = !category ? "Все" : category;
-  const [activeCategory, setActiveCategory] = useState<string | undefined>(
-    initialState,
-  );
-
-  useEffect(() => {
-    getProducts();
-  }, []);
-
-  const { results = [] } = products;
-
-  const filteredItems = useMemo(() => {
-    if (activeCategory === "Все") return results;
-    return results.filter((item) => item.categorie === activeCategory);
-  }, [results, activeCategory]);
-
   return (
     <div className="my-20">
       <Wrapper>
-        <div className="w-full flex flex-col justify-center gap-10">
-          <CategoryFilters
-            active={activeCategory}
-            onChange={setActiveCategory}
-          />
-
-          {isLoading ? (
-            <>Loading...</>
-          ) : (
-            <div className="flex flex-wrap gap-5">
-              {filteredItems.map((item) => (
-                <ItemCard item={item} key={item.id} />
-              ))}
-            </div>
-          )}
-        </div>
+        <Suspense>
+          <Products />
+        </Suspense>
       </Wrapper>
     </div>
   );
---
+++ src/widgets/catalog/ui/Products.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useEffect, useMemo, useState } from "react";
+import { useSearchParams } from "next/navigation";
+
+import ItemCard from "@/src/features/ItemCard";
+import { useProductsStore } from "@/src/entities/catalog/model/store";
+import { CategoryFilters } from "@/src/widgets/categoriesFilter/ui/CategoryFilters";
+
+export const Products = () => {
+  const { products, getProducts, isLoading } = useProductsStore();
+  const searchParams = useSearchParams();
+
+  const category = searchParams?.get("category");
+  const initialState = !category ? "Все" : category;
+  const [activeCategory, setActiveCategory] = useState<string | undefined>(
+    initialState,
+  );
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const { results } = products;
+
+  const filteredItems = useMemo(() => {
+    if (activeCategory === "Все") return results;
+    return results.filter((item) => item.categorie === activeCategory);
+  }, [results, activeCategory]);
+  return (
+    <div className="w-full flex flex-col justify-center gap-10">
+      <CategoryFilters active={activeCategory} onChange={setActiveCategory} />
+
+      {isLoading ? (
+        <>Loading...</>
+      ) : (
+        <div className="flex flex-wrap gap-5">
+          {filteredItems.map((item) => (
+            <ItemCard item={item} key={item.id} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
