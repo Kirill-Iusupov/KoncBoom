464852b2d9a9212ad497b2d3fa7bfeab0368edd3
AprioriDev <apriorifox.dev@gmail.com>
2026-07-10 11:54:25 +0600

    complete: filter products on catalog page, item card; on progress: promotions

---
+++ src/pages/catalog/model/store.ts
@@ -0,0 +1,41 @@
+import { api } from "@/src/shared/api/api";
+import { Product, ResponseData } from "@/src/shared/model/types";
+import { create } from "zustand";
+
+const initialProducts: ResponseData<Product> = {
+  count: 0,
+  next: null,
+  prev: null,
+  results: [],
+};
+
+interface ICatalogStore {
+  products: ResponseData<Product>;
+  isLoading: boolean;
+  error: string | null;
+  getProducts: () => Promise<void>;
+}
+
+export const useProductsStore = create<ICatalogStore>((set) => ({
+  products: initialProducts,
+  isLoading: true,
+  error: null,
+  getProducts: async () => {
+    set({ isLoading: true, error: null });
+
+    try {
+      const res = await api.get("/catalog/products");
+
+      if (res.statusText !== "OK") {
+        throw new Error();
+      }
+
+      set({
+        products: res.data,
+      });
+    } catch (error) {
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+}));
